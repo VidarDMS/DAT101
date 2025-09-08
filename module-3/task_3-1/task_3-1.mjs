0fff03211a4384943c3885f353410da47ad2dcc3
VidarDMS <vstran03@uia.no>
2025-09-08 13:19:06 +0200

    endring igjen.

--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -4,19 +4,31 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
-}
+let wakeUpTime = 8;
+printOut("Wake up time = " + (wakeUpTime));
+if (wakeUpTime === 7) {
+  printOut("I can take the bus to school");
+} else if ( wakeUpTime === 6) {
+  printOut("I have to take the car to school.");
+} else {
+  printOut("I can take can the train to school");
+} 
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let number = 1;
+printOut("value = " + number);
+if (5 > 0) {
+  printOut("The value is positive");
+} else if (5 < 0) {
+  printOut("The value is negative");
+}
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
