b1cc5de421d67182c659cff9337265732875f52e
VidarDMS <vstran03@uia.no>
2025-09-21 20:19:54 +0200

    Egenøving. Presise svar ligger i 2.2 ferdig og 3.1 ferdig

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -4,125 +4,110 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("2 + 3 * (2 -4) * 6 = " + (2 + 3 * (2 -4) * 6));
+const mathResult = (2 + 3 * (2 - 4) * 6);
+printOut(mathResult);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const meters = 25.34;
-const answer = meters * 39.37;
-printOut("25 meters and 34 centimeter = " + (answer).toFixed(2) + " inches");
+const inch = 25.4;
+const metersToMillimeters = ((25 * 1000) + 340);
+const metersToInches = metersToMillimeters / inch;
+const AnswerToNumberOfInches = metersToInches.toFixed(2);
+printOut(AnswerToNumberOfInches);
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const day = 3, hours = 12, min = 14, seconds = 45;
-const totalMinute = (day * 24 * 60) + (hours * 60) + min + (seconds / 60);
-printOut("3 days, 12 hours, 14 minutes and 45 seconds is " + totalMinute + " minutes");
+// 3 dager 12 timer 14 minutter og 45 sekunder til minutter.
+const numberOfDaysToMinuttes = ((24*60)*3); // antall minutter er 4320 minutter
+const numberOfHourstoMinutes = (12*60); // antall minutter er 720 minutter
+const numberOfMinutesToMinutes = 14;
+const numberOfSecondsToMinutes = (45/60); //0.75 minutter
+const numberOfMinutesInTotal = (numberOfDaysToMinuttes + numberOfHourstoMinutes + numberOfMinutesToMinutes + numberOfSecondsToMinutes);
+printOut(numberOfMinutesInTotal);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-/* Jeg vet dette ikke ser ideelt ut, men sånn fikk jeg oversikt, og så kan du se hvordan hodet mitt fungerer*/
-const totalminutes = 6322.52;
-
-const days1 = Math.floor(totalminutes / 1440);
-const afterdays = totalminutes % 1440;
-
-const hours1 = Math.floor(afterdays / 60);
-const afterhours = afterdays % 60;
-
-const minutes1 = Math.floor(afterhours);
-
-const seconds1 = Math.floor((afterhours - minutes1) * 60 );
-
-printOut(totalminutes + " minutes is " + days1 + " days, " + hours1 + " hours " + minutes1 + " minutes and "  + seconds1 + " seconds");
+/* Put your code below here!
+6.322.52 til dager,timer,minutter og sekunder*/
+const totalNumbersOfMinutes = 6322.52
+const totalDays = Math.floor(totalNumbersOfMinutes / 1440);
+const totalHours = Math.floor((totalNumbersOfMinutes % 1440) / 60);
+const totalMinutes = Math.floor(totalNumbersOfMinutes % 60);
+const totalSeconds = Math.floor((totalNumbersOfMinutes % 1) * 60);
+printOut(totalDays + "dager" + totalHours + "timer" + totalMinutes + "minutter" + totalSeconds + "sekunder");
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-let valutakurs = 76/8.6;// = 8.837209302325581
-let svar = 54 * valutakurs;
-let svar2 = 477 / valutakurs;
-printOut("54 USD = " + svar.toFixed(0) + " NOK");
-printOut("477 NOK = " + svar2.toFixed(0) + " USD");
+const oneUsdToNok = Number((76/8.6).toFixed(2)); // 8.84 nok er 1 dollar
+const usd = Number(54 * oneUsdToNok);
+const nok = Number(usd / oneUsdToNok);
+printOut(nok);
+printOut(usd);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 /* Denne oppgaven var plutselig på nork. Vet ikke om det var meningen, men du får svarene i en norsk engelsk miks*/
-const setning = "Det er mye mellom himmel og jord som vi ikke forstår."
-let antallTall = setning.length;
-let ord19 = setning.charAt(19);
-let mangeord = setning.substring(35,43);
-let posisjon = setning.indexOf("jord");
-printOut("The text has " + antallTall + " characters");
-printOut("The character at position 19 is: " + ord19);
-printOut("The substring from 35 and 8 places is: " + mangeord);
-printOut('The word "jord" starts at pos ' + posisjon);
+const sentence = "Det er mye mellom himmel og jord som vi ikke forstår."
+const numberofCharacters = (sentence.length)
+const characterPosision = (sentence.charAt("19"));
+const characterStartToEnd = (sentence.substring(35, 42))
+const characterWord = (sentence.indexOf ("jord"))
+printOut(numberofCharacters);
+printOut(characterPosision);
+printOut(characterStartToEnd);
+printOut(characterWord);
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-let solution = (5 > 3);
-let newSolution = "5 > 3 is " + solution.toString();
 
-let solution2 = "7 >= 7 is " + (7>= 7);
-let newSolution2 = solution2.toString();
-
-let solution3 = ("a" > "b");
-let newSolution3 = '"a" > "b" is ' + solution3.toString();
-
-let solution4 = (1 >"a");
-let NewSolution4 = '"1" > "a" is ' + solution4.toString();
-
-let solution5 = ("2500" < "abcd");
-let newSolution5 = '"2500" < "abcd" ' + solution5.toString(); // hvorfor blir ikke denne true
-
-let solution6 = ("arne" !== "thomas");
-let newSolution6 = '"arne" !== "thomas" ' + solution6.toString();
-
-let solution7 = (2 === 5);
-let newSolution7 = "(2 === 5) === true is " + solution7.toString();
-
-let solution8 = ("abcd" > "bcd");
-let newSolution8 = '("abcd" > "bcd") ' + solution8.toString() + " is true";
-printOut(newSolution);
-printOut(newSolution2);
-printOut(newSolution3);
-printOut(NewSolution4);
-printOut(newSolution5);
-printOut(newSolution6);
-printOut(newSolution7);
-printOut(newSolution8);
+const solutions= (5 > 3);
+const solutionsTwo = (7>= 7);
+const solutionsThree = ("a" > "b");
+const solutionsFour = (1 < "a");
+const solutionFive = ("2500" < "abcd");
+const solutionSix = ("arne" !== "thomas");
+const solutionSeven = (2 === 5);
+const solutionEight = ("abcd" > "bcd");
+printOut(solutions);
+printOut(solutionsTwo);
+printOut(solutionsThree);
+printOut(solutionsFour);
+printOut(solutionFive);
+printOut(solutionSix);
+printOut(solutionSeven);
+printOut(solutionEight);
 printOut(newLine);
 
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-let text = "254";
-let number11 = '"254" = ' + parseFloat(text)
-let text2 = "57.23";
-let number12 = '"57.23" = ' + parseFloat(text2);
-let text3 = ("25");
-let number13 = '" 25 kroner" = ' + parseFloat(text3);
-printOut(number11); 
-printOut(number12);
-printOut("kroner " + number13);
+let tekst1 = "254";
+const number1 = parseInt(tekst1);
+let tekst2 = "57.23";
+const number2 = parseFloat(tekst2);
+let tekst3 = "25 kroner";
+const number3 = parseFloat(tekst3);
+printOut(number1);
+printOut(number2);
+printOut(number3);
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const r = (Math.floor)(Math.random() * 360 + 1);
-printOut("Random number between 1 og 360: " + r);
-printOut("Math.ceil (Math.random() * 360) = 305") /*Dette er hva din print sa derfor la jeg den til.*/
+let r = Math.floor(Math.random() * 360 +1 );
+printOut(r);
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const totaldays = 131;
-let weeks2 = Math.floor(totaldays / 7);
-let days2 = (totaldays % 7);
-printOut(totaldays + " days " + weeks2 + " weeks and " + days2 + " days.");
+const days= 131;
+const weeks = Math.floor(days/7);
+const days2 = (days%7);
+printOut(days + " dager er " + weeks + " uker " + days2 + " uker");
 printOut(newLine);
--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -3,115 +3,106 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Task 1, 2 and 3");
-let wakeUpTime = 8;
-printOut("Wake up time = " + (wakeUpTime));
-if (wakeUpTime === 7) {
-  printOut("I can take the bus to school");
-} else if ( wakeUpTime === 6) {
-  printOut("I have to take the car to school.");
+let wakeUpTime = 7;
+if (wakeUpTime == 6) {
+  printOut("I can take the train");
+} else if (wakeUpTime === 7) {
+  printOut("I can catch the bus to school");
 } else {
-  printOut("I can take can the train to school");
-} 
+  printOut(" I must take the car");
+}
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-let number = 1;
-printOut("value = " + number);
-if (5 > 0) {
-  printOut("The value is positive");
-} else if (5 < 0) {
-  printOut("The value is negative");
+const number = 5;
+if (number < 5) {
+  printOut("positive");
+} else if (number > 5){
+  printOut("negative");
 } else {
-  console.log("The number is zero");
+  printOut("zero")
 }
+
 
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 6, 7----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const pictureSize = Math.floor(Math.random() * 8 + 1)
-if (pictureSize <= 3) {
-  printOut(pictureSize + " The image is too small"); //Jeg får feilmelding når jeg legger til tekst foran pictureSize
-} else if (pictureSize >= 6) {
-  printOut(pictureSize + " Size matters")
-} else {
-  printOut(pictureSize + " Thank you"); //oppgave 7 er denne linjen
+const pictureSize = Math.floor(Math.random() * 8) + 1;
+if (pictureSize < 4) {
+  printOut("Size is to small");
+} else if (pictureSize > 6) {
+  printOut("Image is to large");
+} 
+else {
+  printOut("Thank you");
 }
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const monthList =["January", "February", "Mars", "April", "Mai",
-"Jun", "Juli", "August", "September", "October", "November", "December"];
+const monthList = ["January", "February", "March", "April", "Mai", "June", "Juli", "August", "September", "October", "November", "December"];
 const noOfMonth = monthList.length;
 const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
-if (monthName.includes("r")){
+if (monthName.includes ("r")) {
   printOut("You must take vitamin D");
 } else {
-  printOut("You do not have to take vitamin D");
+  printOut("You do not need to take vitamin D");
 }
-printOut("Replace this with you answer!");
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const monthList1 =["January", "February", "Mars", "April", "Mai",
-"Jun", "Juli", "August", "September", "October", "November", "December"];
+
+const monthList1 = ["January", "February", "March", "April", "Mai", "June", "Juli", "August", "September", "October", "November", "December"];
 const noOfMonth1 = monthList1.length;
 const monthName1 = monthList1[Math.floor(Math.random() * noOfMonth1)];
-
 switch (monthName1) {
   case "January":
-  case "Mars":
+  case "March":
   case "Mai":
   case "Juli":
   case "August":
   case "October":
   case "December":
-  printOut(monthName1 + " har 31 dager i seg");
-break
+    printOut(monthName1 + " har 31 dager");
+    break
   case "February":
-  printOut(monthName1 + " har 28 - 29 dager i seg.");
-break
+    printOut(monthName1 + " har 28 eller 29 dager i seg");
+    break
   case "April":
-  case "Jun":
+  case "June":
   case "September":
   case "November":
-  printOut(monthName1 + " har 30 dager i seg");
-break
+    printOut(monthName1 + " har 30 dager i seg");
 }
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-const monthList2 =["January", "February", "Mars", "April", "Mai",
-"Jun", "Juli", "August", "September", "October", "November", "December"];
-const noOfMonth2 = monthList2.length;
-const monthName2 = monthList2[Math.floor(Math.random() * noOfMonth2)];
-
+const monthList2 = ["January", "February", "March", "April", "Mai", "June", "Juli", "August", "September", "October", "November", "December"];
+const noOfMonth2 = monthList1.length;
+const monthName2 = monthList1[Math.floor(Math.random() * noOfMonth1)];
 switch (monthName2) {
   case "January":
   case "February":
-  case "April":
-  case "Mai":
-  case "Jun":
+  case "June":
+  case "September":
+  case "November":
   case "Juli":
   case "August":
-  case "September":
   case "October":
-  case "November":
   case "December":
-  printOut(monthName2 + " Open");
-break
+    printOut(monthName2 + " open");
+    break
   case "April":
-  printOut(monthName2 + " Open, but use the entrance is next door");
-break
-  case "Mars":
-  case "Mai":
-    printOut(monthName2 + " Closed")
+    printOut(monthName2 + " Open, but enterance is next door");
+    break
+case "March":
+case "Mai":
+    printOut(monthName2 + " closed");
 }
 printOut(newLine);
