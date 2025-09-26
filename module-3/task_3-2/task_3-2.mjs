650e361874a906ee539e20fcc0e5a34295c70fb8
VidarDMS <vstran03@uia.no>
2025-09-26 20:01:30 +0200

    3.2 under arbeid oppgave 6 ferdig

--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -5,10 +5,13 @@ printOut("--- Part 1 -----------------------------------------------------------
 /* Put your code below here!*/
 let txt = "";
 let txt2 = "";
+
 for(let i = 1, j = 10; i <=10; i++, j--) {
     txt += " " + i.toString();
     txt2 += " " + j.toString();
-} printOut(txt);
+} 
+
+printOut(txt);
 printOut(txt2);
 
 printOut(newLine);
@@ -21,6 +24,7 @@ let winningNumber = Math.floor(Math.random() * 60 + 1);
 while (winningNumber !== number) {
 winningNumber = Math.floor(Math.random() * 60 + 1);
 }
+
 printOut("You win! The number was " + winningNumber);
 
 
@@ -28,29 +32,38 @@ printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+const part3Time = Date.now();
+const part3GuessNumber = 353555
+let part3Random;
+let part3Count = 0;
+do{
+    part3Random = Math.floor( Math.random() * 1000000 + 1);
+    part3Count++;
+}while( part3Random !== part3GuessNumber );
 
-let number2 = 45;
-let timeStarted = Date.now();
-let winningNumber2 = Math.floor(Math.random() * 1000000 + 1);
-let attempts = 1;
+printOut(" Number guesses: " + part3Count);
+const part3DeltaTime = Date.now() - part3Time;
+printOut("Number of mSec: " + part3DeltaTime);
 
-while (winningNumber2 !== number2) {
-    winningNumber2 = Math.floor(Math.random() * 1000000 + 1);
-    attempts++;
-}
-
-let timeStopped = Date.now();
-let elapsed = timeStopped - timeStarted;
-
-printOut("You win! The number was " + winningNumber2);
-printOut("Attempts: " + attempts);
-printOut("Time (ms): " + elapsed);
-
-printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Use a "for" loop and a "while" loop to find all prime numbers greater than 1 and less than 200*/
+let txtprimes = "";
 
+for(let i = 1; i <200; i++) {
+    let j = i - 1;
+    let isPrime = true;
+    while(j > 1 && isPrime) {
+        let rest = i % j;
+        isPrime = rest != 0;
+        j--;
+    }
+    if(isPrime) {
+    txtprimes += " " + i;
+    }
+    } 
+
+printOut(txtprimes);
 
 printOut(newLine);
 
@@ -70,26 +83,35 @@ printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+for ( let i = 1; i <= 5; i++) {
+        const number6 = Math.floor(Math.random() * 236 + 1); 
+        const percent = (number6 / 236 * 100);
+
+if(percent <= 40) {
+    printOut("Grade F");
+} else if (percent <= 52) {
+    printOut("Grade E");
+} else if (percent <= 64){
+    printOut("Grade D");
+} else if (percent <= 76){
+    printOut("Grade C");
+} else if (percent <= 88) {
+    printOut("Grade B");
+} else {
+    printOut("Grade A");
+}
+} 
+
+printOut(i);
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+
+
 printOut(newLine);
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
 
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-/* Task 10*/
-printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
