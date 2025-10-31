2fc3abf837a4f1a3dd326b359868109654f34c01
Vidar Strandås <vstran03@uia.no>
2025-10-31 13:29:53 +0100

    skole 31.10

--- module-4/task_4-2/task_4-2.mjs
+++ module-4/task_4-2/task_4-2.mjs
@@ -3,17 +3,40 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const part1Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let part1Text = "";
+for (let i = 0; i < part1Array.length; i++){
+    const value = part1Array[i]; // Den benytter seg av hver eneste index
+    if (i === part1Array.length - 1) {
+        part1Text += value.toString() + ".";
+    } else {
+    part1Text += value.toString() + ", ";
+}
+}
+printOut(part1Text);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const part2Text = part1Array.join(", ");
+printOut (part2Text);
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const part3Greeting = "Hello there, how are you?";
+const greetingArray  = part3Greeting.split(" ");
+
+let part3Text = "";
+for (let i = 0; i < greetingArray.length; i++) {
+    const word = greetingArray[i];
+    // Here you can remove any unwanted characters e,g: , ? et.
+    part3Text += "Index: " + i.toString() + " = " + word + newLine; 
+}
+printOut(part3Text);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
